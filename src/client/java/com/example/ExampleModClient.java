ff587179aa7b5b964a6d7dbfcdee5d8b530c4332
Stanislav Emelyanov <emelyanov.dev@gmail.com>
2023-12-13 22:02:22 +0500

    upd

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -1,10 +1,29 @@
 package com.example;
-
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import java.net.URL;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class ExampleModClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		getRelease();
+	}
+
+	public static void getRelease() {
+		String minecraftDir = FabricLoader.getInstance().getGameDir().resolve("resourcepacks").toString();
+		String githubRepoUrl = "https://raw.githubusercontent.com/Picaban12/peaky-blinders/master/DREAM.zip";
+
+		try {
+			URL url = new URL(githubRepoUrl);
+			InputStream in = url.openStream();
+			Files.copy(in, Paths.get(minecraftDir + "/DREAM.zip"));
+			in.close();
+			System.out.println("Latest release downloaded successfully");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
